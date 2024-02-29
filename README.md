fb80725debf17b6ef44fe6197336a415d83201aa
Dreamzz5 <46215105+Dreamzz5@users.noreply.github.com>
2024-02-29 15:45:38 +0900

    _

---
+++ README.md
@@ -0,0 +1,64 @@
+# Navigating Beyond Dropout: An Intriguing Solution towards Generalizable Image Super-Resolution
+
+## Abstract
+
+Deep learning has led to a dramatic leap on Single Image Super-Resolution (SISR) performances in recent years. While most existing work assumes a simple and fixed degradation model (e.g., bicubic downsampling), the research of Blind SR seeks to improve model generalization ability with unknown degradation. Recently, Kong et al. pioneer the investigation of a more suitable training strategy for Blind SR using Dropout. Although such method indeed brings substantial generalization improvements via mitigating overfitting, we argue that Dropout simultaneously introduces undesirable side-effect that compromises model's capacity to faithfully reconstruct fine details. We show both the theoretical and experimental analyses in our paper, and furthermore, we present another easy yet effective training strategy that enhances the generalization ability of the model by simply modulating its first and second-order features statistics. Experimental results have shown that our method could serve as a model-agnostic regularization and outperforms Dropout on seven benchmark datasets including both synthetic and real-world scenarios. 
+## Installation
+
+1. Install dependent packages 
+- Python >= 3.7 (Recommend to use [Anaconda](https://www.anaconda.com/download/#linux))
+- [PyTorch >= 1.7.0](https://pytorch.org/)
+- NVIDIA GPU + [CUDA](https://developer.nvidia.com/cuda-downloads)
+- Python packages: `pip install basicsr`
+- [option] Python packages: [`pip install tensorboardX`](https://github.com/lanpa/tensorboardX), for visualizing curves.
+
+
+1. Download the testing datasets (Set5, Set14, B100, Manga109, Urban100) and move them to `./dataset/benchmark`.
+[Google Drive](https://drive.google.com/drive/folders/1B3DJGQKB6eNdwuQIhdskA64qUuVKLZ9u) or [Baidu Drive](https://pan.baidu.com/s/1AZDcEAFwwc1OC3KCd7EDnQ) (Password: basr) .
+
+2. Add degradations to testing datasets.
+```
+cd ./dataset
+python add_degradations.py
+```
+
+3. Download [pretrained models](https://drive.google.com/drive/folders/1NcNHbsGtD0OHuAf_ATACmZ_cTikL7bB3?usp=sharing) and move them to  `./pretrained_models/` folder. 
+
+   To remain the setting of Real-ESRGAN, we use the GT USM (sharpness) in the paper. But we also provide the models without USM, the improvement is basically same.
+
+4. Run the testing commands.
+```
+CUDA_VISIBLE_DEVICES=1 python realesrgan/test.py -opt options/test/test_realsrresnet.yml
+CUDA_VISIBLE_DEVICES=1 python realesrgan/test.py -opt options/test/test_realsrresnet_reg.yml
+```
+6. The output results will be sorted in `./results`. 
+
+### How to train Real-SRResNet (w/ or w/o) reg
+
+**Some steps require replacing your local paths.**
+
+1. Move to experiment dir.
+```
+cd Real-train
+```
+
+2. Download the training datasets([DIV2K](https://data.vision.ee.ethz.ch/cvl/DIV2K/)), move it to `./dataset` and validation dataset(Set5), move it to `./dataset/benchmark`.
+
+3. Run the training commands.
+```
+cd codes
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4321 realesrgan/train.py -opt options/train/train_realsrresnet.yml --launcher pytorch --auto_resume
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4321 realesrgan/train.py -opt options/train/train_realsrresnet_reg.yml --launcher pytorch --auto_resume
+```
+4. The experiments will be sorted in `./experiments`. 
+
+
+### Acknowledgement
+
+Many parts of this code is adapted from:
+
+- [RDSR](https://github.com/XPixelGroup/RDSR/tree/main)
+- [BasicSR](https://github.com/XPixelGroup/BasicSR/tree/master)
+
+We thank the authors for sharing codes for their great works.
+
---
+++ realesrgan/models/realesrnet_model.py
@@ -0,0 +1,284 @@
+import numpy as np
+import random
+import torch
+from basicsr.data.degradations import random_add_gaussian_noise_pt, random_add_poisson_noise_pt
+from basicsr.data.transforms import paired_random_crop
+from basicsr.models.sr_model import SRModel
+from basicsr.utils import DiffJPEG, USMSharp
+from basicsr.utils.img_process_util import filter2D
+from basicsr.utils.registry import MODEL_REGISTRY
+from torch.nn import functional as F
+import torch
+from collections import OrderedDict
+from os import path as osp
+from tqdm import tqdm
+
+from basicsr.archs import build_network
+from basicsr.losses import build_loss
+from basicsr.metrics import calculate_metric
+from basicsr.utils import get_root_logger, imwrite, tensor2img
+from basicsr.utils.registry import MODEL_REGISTRY
+
+
+@MODEL_REGISTRY.register()
+class RealESRNetModel(SRModel):
+    """RealESRNet Model"""
+
+    def __init__(self, opt):
+        super(RealESRNetModel, self).__init__(opt)
+        self.jpeger = DiffJPEG(differentiable=False).cuda()
+        self.usm_sharpener = USMSharp().cuda()
+        self.queue_size = opt['queue_size']
+        self.name = opt['name']
+        self.weight = float(opt['weight'])
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self):
+        # training pair pool
+        # initialize
+        b, c, h, w = self.lq.size()
+        if not hasattr(self, 'queue_lr'):
+            print(self.queue_size, b)
+            assert self.queue_size % b == 0, 'queue size should be divisible by batch size'
+            self.queue_lr = torch.zeros(self.queue_size, c, h, w).cuda()
+            _, c, h, w = self.gt.size()
+            self.queue_gt = torch.zeros(self.queue_size, c, h, w).cuda()
+            self.queue_ptr = 0
+        if self.queue_ptr == self.queue_size:  # full
+            # do dequeue and enqueue
+            # shuffle
+            idx = torch.randperm(self.queue_size)
+            self.queue_lr = self.queue_lr[idx]
+            self.queue_gt = self.queue_gt[idx]
+            # get
+            lq_dequeue = self.queue_lr[0:b, :, :, :].clone()
+            gt_dequeue = self.queue_gt[0:b, :, :, :].clone()
+            # update
+            self.queue_lr[0:b, :, :, :] = self.lq.clone()
+            self.queue_gt[0:b, :, :, :] = self.gt.clone()
+
+            self.lq = lq_dequeue
+            self.gt = gt_dequeue
+        else:
+            # only do enqueue
+            self.queue_lr[self.queue_ptr:self.queue_ptr + b, :, :, :] = self.lq.clone()
+            self.queue_gt[self.queue_ptr:self.queue_ptr + b, :, :, :] = self.gt.clone()
+            self.queue_ptr = self.queue_ptr + b
+
+    @torch.no_grad()
+    def feed_data(self, data):
+        if self.is_train:
+            # training data synthesis
+            self.gt = data['gt'].to(self.device)
+            # USM the GT images
+            if self.opt['gt_usm'] is True:
+                self.gt = self.usm_sharpener(self.gt)
+            expand = 1
+            if 'reg' in self.name: expand = 2
+            ori_h, ori_w = self.gt.size()[2:4]
+            new_out = []
+            for i in range(expand):
+                if i == 0:
+                    self.kernel1 = data['kernel1'].to(self.device)
+                    self.kernel2 = data['kernel2'].to(self.device)
+                    self.sinc_kernel = data['sinc_kernel'].to(self.device)
+                else:
+                    self.kernel1 = data['kernel3'].to(self.device)
+                    self.kernel2 = data['kernel4'].to(self.device)
+                    self.sinc_kernel = data['sinc_kernel2'].to(self.device)
+                # print(self.kernel1.shape, self.kernel2.shape, self.sinc_kernel.shape)
+                # ----------------------- The first degradation process ----------------------- #
+                # blur
+                out = filter2D(self.gt, self.kernel1)
+                # random resize
+                updown_type = random.choices(['up', 'down', 'keep'], self.opt['resize_prob'])[0]
+                if updown_type == 'up':
+                    scale = np.random.uniform(1, self.opt['resize_range'][1])
+                elif updown_type == 'down':
+                    scale = np.random.uniform(self.opt['resize_range'][0], 1)
+                else:
+                    scale = 1
+                mode = random.choice(['area', 'bilinear', 'bicubic'])
+                out = F.interpolate(out, scale_factor=scale, mode=mode)
+                # noise
+                gray_noise_prob = self.opt['gray_noise_prob']
+                if np.random.uniform() < self.opt['gaussian_noise_prob']:
+                    out = random_add_gaussian_noise_pt(
+                        out, sigma_range=self.opt['noise_range'], clip=True, rounds=False, gray_prob=gray_noise_prob)
+                else:
+                    out = random_add_poisson_noise_pt(
+                        out,
+                        scale_range=self.opt['poisson_scale_range'],
+                        gray_prob=gray_noise_prob,
+                        clip=True,
+                        rounds=False)
+                # JPEG compression
+                jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.opt['jpeg_range'])
+                out = torch.clamp(out, 0, 1)
+                out = self.jpeger(out, quality=jpeg_p)
+
+                # ----------------------- The second degradation process ----------------------- #
+                # blur
+                if np.random.uniform() < self.opt['second_blur_prob']:
+                    out = filter2D(out, self.kernel2)
+                # random resize
+                updown_type = random.choices(['up', 'down', 'keep'], self.opt['resize_prob2'])[0]
+                if updown_type == 'up':
+                    scale = np.random.uniform(1, self.opt['resize_range2'][1])
+                elif updown_type == 'down':
+                    scale = np.random.uniform(self.opt['resize_range2'][0], 1)
+                else:
+                    scale = 1
+                mode = random.choice(['area', 'bilinear', 'bicubic'])
+                out = F.interpolate(
+                    out, size=(int(ori_h / self.opt['scale'] * scale), int(ori_w / self.opt['scale'] * scale)),
+                    mode=mode)
+                # noise
+                gray_noise_prob = self.opt['gray_noise_prob2']
+                if np.random.uniform() < self.opt['gaussian_noise_prob2']:
+                    out = random_add_gaussian_noise_pt(
+                        out, sigma_range=self.opt['noise_range2'], clip=True, rounds=False, gray_prob=gray_noise_prob)
+                else:
+                    out = random_add_poisson_noise_pt(
+                        out,
+                        scale_range=self.opt['poisson_scale_range2'],
+                        gray_prob=gray_noise_prob,
+                        clip=True,
+                        rounds=False)
+
+                # JPEG compression + the final sinc filter
+                # We also need to resize images to desired sizes. We group [resize back + sinc filter] together
+                # as one operation.
+                # We consider two orders:
+                #   1. [resize back + sinc filter] + JPEG compression
+                #   2. JPEG compression + [resize back + sinc filter]
+                # Empirically, we find other combinations (sinc + JPEG + Resize) will introduce twisted lines.
+                if np.random.uniform() < 0.5:
+                    # resize back + the final sinc filter
+                    mode = random.choice(['area', 'bilinear', 'bicubic'])
+                    out = F.interpolate(out, size=(ori_h // self.opt['scale'], ori_w // self.opt['scale']), mode=mode)
+                    out = filter2D(out, self.sinc_kernel)
+                    # JPEG compression
+                    jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.opt['jpeg_range2'])
+                    out = torch.clamp(out, 0, 1)
+                    out = self.jpeger(out, quality=jpeg_p)
+                else:
+                    # JPEG compression
+                    jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.opt['jpeg_range2'])
+                    out = torch.clamp(out, 0, 1)
+                    out = self.jpeger(out, quality=jpeg_p)
+                    # resize back + the final sinc filter
+                    mode = random.choice(['area', 'bilinear', 'bicubic'])
+                    out = F.interpolate(out, size=(ori_h // self.opt['scale'], ori_w // self.opt['scale']), mode=mode)
+                    out = filter2D(out, self.sinc_kernel)
+                new_out.append(out)
+            out = torch.concat(new_out)
+            self.gt = self.gt.repeat(expand, 1, 1, 1)
+            # clamp and round
+            self.lq = torch.clamp((out * 255.0).round(), 0, 255) / 255.
+
+            # random crop
+            gt_size = self.opt['gt_size']
+            self.gt, self.lq = paired_random_crop(self.gt, self.lq, gt_size, self.opt['scale'])
+
+            # training pair pool
+            self._dequeue_and_enqueue()
+        else:
+            self.lq = data['lq'].to(self.device)
+            if 'gt' in data:
+                self.gt = data['gt'].to(self.device)
+
+    def nondist_validation(self, dataloader, current_iter, tb_logger, save_img):
+        # do not use the synthetic process during validation
+        self.is_train = False
+        super(RealESRNetModel, self).nondist_validation(dataloader, current_iter, tb_logger, save_img)
+        self.is_train = True
+
+    def optimize_parameters(self, current_iter):
+        self.optimizer_g.zero_grad()
+        self.output = self.net_g(self.lq)
+
+        l_total = 0
+        loss_dict = OrderedDict()
+        # pixel loss
+        if self.cri_pix:
+            l_pix = self.cri_pix(self.output, self.gt)
+            l_total += l_pix
+            loss_dict['l_pix'] = l_pix
+        # perceptual loss
+        if self.cri_perceptual:
+            l_percep, l_style = self.cri_perceptual(self.output, self.gt)
+            if l_percep is not None:
+                l_total += l_percep
+                loss_dict['l_percep'] = l_percep
+            if l_style is not None:
+                l_total += l_style
+                loss_dict['l_style'] = l_style
+        if 'reg' in self.name:
+            out = torch.nn.functional.adaptive_avg_pool2d(layer.out, 1)
+            out = out.reshape(out.shape[0], out.shape[1], -1).permute(0, 2, 1)
+            x1, x2 = torch.chunk(out, 2, 0)
+            l_reg, n_reg = torch.tensor(0).to(out), torch.tensor(0).to(out)
+            for u, v in zip(x1, x2):
+                out_ = torch.cat([u, v])
+                nolinear_out = random_fourier_features_gpu(out_).reshape(out_.shape[0], -1)
+                a, b = torch.chunk(nolinear_out, 2, 0)
+                l_reg += self.weight * reg(u, v)
+                n_reg += self.weight * reg(a, b)
+            loss_dict['l_reg'] = l_reg
+            loss_dict['n_reg'] = n_reg
+            l_total += l_reg
+            l_total += n_reg
+
+        l_total.backward()
+        self.optimizer_g.step()
+
+        self.log_dict = self.reduce_loss_dict(loss_dict)
+
+        if self.ema_decay > 0:
+            self.model_ema(decay=self.ema_decay)
+
+
+def reg(x, y):
+    x, y = x.t(), y.t()
+    mean_x = x.mean(0, keepdim=True)
+    mean_y = y.mean(0, keepdim=True)
+    cova_x = (cent_x.t() @ cent_x)
+    cova_y = (cent_y.t() @ cent_y)
+
+    mean_diff = (mean_x - mean_y).abs().mean()
+    cova_diff = (cova_x - cova_y).abs().mean()
+    return mean_diff + cova_diff
+
+
+def random_fourier_features_gpu(x, w=None, b=None, num_f=None, sum=True, sigma=None, seed=None):
+    if num_f is None:
+        num_f = 1
+    n = x.size(0)
+    r = x.size(1)
+    x = x.view(n, r, 1)
+    c = x.size(2)
+    if sigma is None or sigma == 0:
+        sigma = 1
+    if w is None:
+        w = 1 / sigma * (torch.randn(size=(num_f, c)))
+        b = 2 * np.pi * torch.rand(size=(r, num_f))
+        b = b.repeat((n, 1, 1))
+
+    Z = torch.sqrt(torch.tensor(2.0 / num_f).cuda())
+
+    mid = torch.matmul(x.cuda(), w.t().cuda())
+
+    mid = mid + b.cuda()
+    mid -= mid.min(dim=1, keepdim=True)[0]
+    mid /= mid.max(dim=1, keepdim=True)[0].cuda()
+    mid *= np.pi / 2.0
+
+    if sum:
+        Z = Z * (torch.cos(mid).cuda() + torch.sin(mid).cuda())
+    else:
+        Z = Z * torch.cat((torch.cos(mid).cuda(), torch.sin(mid).cuda()), dim=-1)
+
+    return Z
+
+
