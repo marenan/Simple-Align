f92d644f9a4e5b03b81072ff1b3f6c085c76aab0
Dreamzz5 <46215105+Dreamzz5@users.noreply.github.com>
2024-02-29 16:19:15 +0900

    Update realesrnet_model.py

--- realesrgan/models/realesrnet_model.py
+++ realesrgan/models/realesrnet_model.py
@@ -242,10 +242,8 @@ class RealESRNetModel(SRModel):
 def reg(x, y):
     mean_x = x.mean(0, keepdim=True)
     mean_y = y.mean(0, keepdim=True)
-    cent_x = x - mean_x
-    cent_y = y - mean_y
-    cova_x = (cent_x.t() @ cent_x)
-    cova_y = (cent_y.t() @ cent_y)
+    cova_x = (x.t() @ x)
+    cova_y = (y.t() @ y)
 
     mean_diff = (mean_x - mean_y).pow(2).mean()
     cova_diff = (cova_x - cova_y).pow(2).mean()
